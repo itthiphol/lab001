a3788efed437a26f2a2beecec2e0a38b92e8a5e3
Itthiphol Eampoonga <42127273+itthiphol@users.noreply.github.com>
2022-10-12 10:03:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <title>PHP - Hello, World!</title>
 </head>
 <body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
-    
+        <h1><?php echo 'Hello, World!'; ?></h1>         
         <h1><?php echo 'Itthiphol-65001234'; ?></h1>
+        <h1><?php echo 'Resume'; ?></h1>
         
 </body>
 </html>
