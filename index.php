8e5ccda6302fe7bcd24f1a1e2f9713000b99295e
Itthiphol Eampoonga <42127273+itthiphol@users.noreply.github.com>
2022-12-01 15:43:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
-</head>
-<body>
-        <h1><?php echo 'Hello, World!'; ?></h1>         
-        <h1><?php echo 'Itthiphol-65001234'; ?></h1>
-        <h1><?php echo 'Resume8888888'; ?></h1>
-        <h1><?php echo 'Resume'; ?></h1>
-        <h1><?php echo 'Resume'; ?></h1>
-        <h1><?php echo 'Resume'; ?></h1>
-        <h1><?php echo 'Resume9999999'; ?></h1>
-        <h1><?php echo 'Resume888888'; ?></h1>
-        
-        
-</body>
-</html>
+<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
